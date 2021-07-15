b8a8b8656ee4e9967a764635968efc79960764c2
Muh-Hasan <muh.hasan.git@gmail.com>
2021-07-15 23:46:22 +0500

    removed unused files & updated gitignore

--- functions/iam/index.d.ts
+++
@@ -1,6 +0,0 @@
-import { CodeWriter, TextWriter } from "@yellicode/core";
-export declare class Iam extends CodeWriter {
-    importIam(output: TextWriter): void;
-    serviceRoleForAppsync(output: TextWriter, apiName: string): void;
-    attachLambdaPolicyToRole(roleName: string): void;
-}
