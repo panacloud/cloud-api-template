d56aeaaeb87f0e9b1386279957cc278cb6c4f19d
Muh-Hasan <muh.hasan.git@gmail.com>
2021-07-15 23:42:15 +0500

    code refactor && trying to removing unused files with extension .js and .d.ts

--- functions/api-manager/index.ts
+++ functions/api-manager/index.ts
@@ -2,16 +2,22 @@ import { CodeWriter, TextWriter } from "@yellicode/core";
 import { TypeScriptWriter } from "@yellicode/typescript";
 
 export class apiManager extends CodeWriter {
-    
-    public importApiManager(output:TextWriter){
-        const ts = new TypeScriptWriter(output);
-        ts.writeImports("panacloud-manager",["PanacloudManager"]);    
-    }
+  public importApiManager(output: TextWriter) {
+    const ts = new TypeScriptWriter(output);
+    ts.writeImports("panacloud-manager", ["PanacloudManager"]);
+  }
 
-    public apiManagerInitializer(output:TextWriter , name:string){
-        const ts = new TypeScriptWriter(output);
-        ts.writeVariableDeclaration({name:"apiManager",typeName:"any",initializer:()=>{
-            ts.writeLine(`new PanacloudManager(this, "${name}APIManager")`)
-        }}, "const")
-    }
+  public apiManagerInitializer(output: TextWriter, name: string) {
+    const ts = new TypeScriptWriter(output);
+    ts.writeVariableDeclaration(
+      {
+        name: "apiManager",
+        typeName: "any",
+        initializer: () => {
+          ts.writeLine(`new PanacloudManager(this, "${name}APIManager")`);
+        },
+      },
+      "const"
+    );
+  }
 }
