abb30d2a7445f65a50d159dd63b1bb464f1fc158
TanzeelTasleem <tanzeeltasleem648@gmail.com>
2021-07-15 15:32:58 +0500

    change string to camelcase

--- functions/utils/class.js
+++ functions/utils/class.js
@@ -3,11 +3,12 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.BasicClass = void 0;
 const core_1 = require("@yellicode/core");
 const typescript_1 = require("@yellicode/typescript");
+const _ = require('lodash');
 class BasicClass extends core_1.CodeWriter {
     initializeClass(name, contents, output) {
         const ts = new typescript_1.TypeScriptWriter(output);
         const classDefinition = {
-            name: name,
+            name: `${_.camelCase(name)}Stack`,
             extends: ["Stack"],
             export: true,
         };
