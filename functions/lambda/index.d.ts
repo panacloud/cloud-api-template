377192cc4a837d509506aa9e328e05a0fea3fff6
Muh-Hasan <muh.hasan.git@gmail.com>
2021-07-15 23:46:22 +0500

    removed unused files & updated gitignore

--- functions/lambda/index.d.ts
+++
@@ -1,6 +0,0 @@
-import { CodeWriter, TextWriter } from "@yellicode/core";
-export declare class Lambda extends CodeWriter {
-    importLambda(output: TextWriter): void;
-    initializeLambda(apiName: string, output: TextWriter): void;
-    addEnvironment(lambda: string, envName: string, value: string): void;
-}
