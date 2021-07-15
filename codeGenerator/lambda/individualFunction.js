cf5624420b45faabcf5d7208d71bf1a21d76203a
TanzeelTasleem <tanzeeltasleem648@gmail.com>
2021-07-15 16:28:37 +0500

    add condition check for object key

--- codeGenerator/lambda/individualFunction.js
+++ codeGenerator/lambda/individualFunction.js
@@ -1,18 +1,23 @@
 "use strict";
+var _a;
 Object.defineProperty(exports, "__esModule", { value: true });
 const templating_1 = require("@yellicode/templating");
 const lambdaFunction_1 = require("../../functions/lambda/lambdaFunction");
 const jsonObj = require(`../../model.json`);
 const { USER_WORKING_DIRECTORY } = jsonObj;
-Object.keys(jsonObj.type.Query).forEach((key) => {
-    templating_1.Generator.generate({ outputFile: `../../../${USER_WORKING_DIRECTORY}/lambda-fns/${key}.ts` }, (writer) => {
-        const lambda = new lambdaFunction_1.LambdaFunction(writer);
-        lambda.helloWorldFunction(key);
+if ((_a = jsonObj === null || jsonObj === void 0 ? void 0 : jsonObj.type) === null || _a === void 0 ? void 0 : _a.Query) {
+    Object.keys(jsonObj.type.Query).forEach((key) => {
+        templating_1.Generator.generate({ outputFile: `../../../${USER_WORKING_DIRECTORY}/lambda-fns/${key}.ts` }, (writer) => {
+            const lambda = new lambdaFunction_1.LambdaFunction(writer);
+            lambda.helloWorldFunction(key);
+        });
     });
-});
-Object.keys(jsonObj.type.Mutation).forEach((key) => {
-    templating_1.Generator.generate({ outputFile: `../../../${USER_WORKING_DIRECTORY}/lambda-fns/${key}.ts` }, (writer) => {
-        const lambda = new lambdaFunction_1.LambdaFunction(writer);
-        lambda.helloWorldFunction(key);
+}
+if (jsonObj.type.Mutation) {
+    Object.keys(jsonObj.type.Mutation).forEach((key) => {
+        templating_1.Generator.generate({ outputFile: `../../../${USER_WORKING_DIRECTORY}/lambda-fns/${key}.ts` }, (writer) => {
+            const lambda = new lambdaFunction_1.LambdaFunction(writer);
+            lambda.helloWorldFunction(key);
+        });
     });
-});
+}
