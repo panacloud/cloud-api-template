2ebad27e2500dc8012ffd153e40f18755fca7105
shariqanwar20 <shariqanwar59@gmail.com>
2021-07-25 16:45:00 +0500

    fixed the bug (added the updated js files)

--- codeGenerator/lambda/initializeLambdaFunction.js
+++ codeGenerator/lambda/initializeLambdaFunction.js
@@ -1,39 +1,58 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const templating_1 = require("@yellicode/templating");
-const typescript_1 = require("@yellicode/typescript");
+// import { TypeScriptWriter } from "@yellicode/typescript";
 const lambdaFunction_1 = require("../../functions/lambda/lambdaFunction");
 const model = require("../../model.json");
 const { USER_WORKING_DIRECTORY } = model;
-templating_1.Generator.generateFromModel({ outputFile: `../../../${USER_WORKING_DIRECTORY}/lambda-fns/main.ts` }, (output, model) => {
-    const ts = new typescript_1.TypeScriptWriter(output);
-    const lambda = new lambdaFunction_1.LambdaFunction(output);
-    for (var key in model.type.Query) {
-        lambda.importIndividualFunction(output, key, `./${key}`);
-    }
-    for (var key in model.type.Mutation) {
-        lambda.importIndividualFunction(output, key, `./${key}`);
-    }
-    ts.writeLine();
-    ts.writeLineIndented(`
-    type Event = {
-        info: {
-          fieldName: string
-       }
-     }`);
-    ts.writeLine();
-    lambda.initializeLambdaFunction(() => {
-        for (var key in model.type.Query) {
-            ts.writeLineIndented(`
-          case "${key}":
-              return await ${key}();
-          `);
-        }
-        for (var key in model.type.Mutation) {
-            ts.writeLineIndented(`
-          case "${key}":
-              return await ${key}();
-          `);
-        }
-    }, output);
-});
+if (model.type.Mutation) {
+    Object.keys(model.type.Mutation).forEach((key) => {
+        templating_1.Generator.generate({ outputFile: `../../../${USER_WORKING_DIRECTORY}/lambda-fns/${key}.ts` }, (writer) => {
+            const lambda = new lambdaFunction_1.LambdaFunction(writer);
+            lambda.initializeLambdaFunction(writer);
+        });
+    });
+}
+if (model.type.Query) {
+    Object.keys(model.type.Query).forEach((key) => {
+        templating_1.Generator.generate({ outputFile: `../../../${USER_WORKING_DIRECTORY}/lambda-fns/${key}.ts` }, (writer) => {
+            const lambda = new lambdaFunction_1.LambdaFunction(writer);
+            lambda.initializeLambdaFunction(writer);
+        });
+    });
+}
+// Generator.generateFromModel(
+//   { outputFile: `../../../${USER_WORKING_DIRECTORY}/lambda-fns/main.ts` },
+//   (output: TextWriter, model: any) => {
+//     const ts = new TypeScriptWriter(output);
+//     const lambda = new LambdaFunction(output);
+//     for (var key in model.type.Query) {
+//       lambda.importIndividualFunction(output, key, `./${key}`);
+//     }
+//     for (var key in model.type.Mutation) {
+//       lambda.importIndividualFunction(output, key, `./${key}`);
+//     }
+//     ts.writeLine();
+//     ts.writeLineIndented(`
+//     type Event = {
+//         info: {
+//           fieldName: string
+//        }
+//      }`);
+//     ts.writeLine();
+//     lambda.initializeLambdaFunction(() => {
+//       for (var key in model.type.Query) {
+//         ts.writeLineIndented(`
+//           case "${key}":
+//               return await ${key}();
+//           `);
+//       }
+//       for (var key in model.type.Mutation) {
+//         ts.writeLineIndented(`
+//           case "${key}":
+//               return await ${key}();
+//           `);
+//       }
+//     }, output);
+//   }
+// );
--- functions/lambda/lambdaFunction.js
+++ functions/lambda/lambdaFunction.js
@@ -4,25 +4,12 @@ exports.LambdaFunction = void 0;
 const core_1 = require("@yellicode/core");
 const typescript_1 = require("@yellicode/typescript");
 class LambdaFunction extends core_1.CodeWriter {
-    initializeLambdaFunction(content, output) {
+    initializeLambdaFunction(output) {
         const ts = new typescript_1.TypeScriptWriter(output);
-        ts.writeLine(`exports.handler = async (event:Event) => {`);
-        ts.writeLine(`switch (event.info.fieldName) {`);
-        ts.writeLine();
-        content();
-        ts.writeLine();
-        ts.writeLine(`}`);
-        ts.writeLine(`}`);
-    }
-    importIndividualFunction(output, name, path) {
-        const ts = new typescript_1.TypeScriptWriter(output);
-        ts.writeImports(path, [name]);
-    }
-    helloWorldFunction(name) {
-        this.writeLineIndented(`
+        ts.writeLineIndented(`
     const AWS = require('aws-sdk');
     
-    export const ${name} = async() => {
+    exports.handler = async() => {
       // write your code here
     }
     `);
