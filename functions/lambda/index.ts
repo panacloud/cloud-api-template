6b9116538000412229b7678529bb60ee5300fc58
shariqanwar20 <shariqanwar59@gmail.com>
2021-07-25 16:25:29 +0500

    made an individual handler function for each query & mutation

--- functions/lambda/index.ts
+++ functions/lambda/index.ts
@@ -7,17 +7,17 @@ export class Lambda extends CodeWriter {
     ts.writeImports("aws-cdk-lib", ["aws_lambda as lambda"]);
   }
 
-  public initializeLambda(apiName: string, output: TextWriter) {
+  public initializeLambda(apiName: string, output: TextWriter, functionName: string) {
     const ts = new TypeScriptWriter(output);
     ts.writeVariableDeclaration(
       {
-        name: `${apiName}_lambdaFn`,
+        name: `${apiName}_lambdaFn_${functionName}`,
         typeName: "lambda.Function",
         initializer: () => {
-          ts.writeLine(`new lambda.Function(this, "${apiName}Lambda", {
-        functionName: "${apiName}Lambda",
+          ts.writeLine(`new lambda.Function(this, "${apiName}Lambda${functionName}", {
+        functionName: "${apiName}Lambda${functionName}",
         runtime: lambda.Runtime.NODEJS_12_X,
-        handler: "main.handler",
+        handler: "${functionName}.handler",
         code: lambda.Code.fromAsset("lambda-fns"),
         memorySize: 1024,
       });`);
@@ -26,9 +26,9 @@ export class Lambda extends CodeWriter {
       "const"
     );
   }
-  public addEnvironment(lambda: string, envName: string, value: string) {
+  public addEnvironment(lambda: string, functionName: string, envName: string, value: string) {
     this.writeLine(
-      `${lambda}_lambdaFn.addEnvironment("${envName}", ${value});`
+      `${lambda}_lambdaFn_${functionName}.addEnvironment("${envName}", ${value});`
     );
   }
 }
