3bf5fd88ccb8e603f658ec6e936274692bf577b4
shariqanwar20 <shariq.anwar@nixorcollege.edu.pk>
2021-07-31 16:42:19 +0500

    fixed path bug in class file #8

--- codeGenerator/class/index.ts
+++ codeGenerator/class/index.ts
@@ -24,7 +24,7 @@ Generator.generateFromModel(
     const manager = new apiManager(output);
     const cls = new BasicClass(output);
     const schema = fs
-      .readFileSync(`../../../graphql/schema.graphql`)
+      .readFileSync(`../../../../graphql/schema.graphql`)
       .toString("utf8");
 
     ts.writeImports("aws-cdk-lib", ["Stack", "StackProps"]);
