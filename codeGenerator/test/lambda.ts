2a2dc44de8df7a39f13838658ca837f0796854a6
TanzeelTasleem <tanzeeltasleem648@gmail.com>
2021-08-01 13:27:19 +0500

    feat: cdk constructs test generation for multiple lambdas added #6

--- codeGenerator/test/lambda.ts
+++ codeGenerator/test/lambda.ts
@@ -7,29 +7,45 @@ import { Lambda } from "../../functions/lambda";
 const jsonObj = require(`../../model.json`);
 const { USER_WORKING_DIRECTORY , API_NAME , LAMBDA_STYLE} = jsonObj;
 
-if(LAMBDA_STYLE === "single lambda") {
-    Generator.generate({outputFile: `../../../${USER_WORKING_DIRECTORY}/test/${USER_WORKING_DIRECTORY}-lambda.test.ts`,},
-    (output: TextWriter) => {
+if(LAMBDA_STYLE) {
+    Generator.generateFromModel({outputFile: `../../../${USER_WORKING_DIRECTORY}/test/${USER_WORKING_DIRECTORY}-lambda.test.ts`,},
+    (output: TextWriter,model:any) => {
       const ts = new TypeScriptWriter(output);
       const testClass = new TestingConstructs(output)
       const iam = new Iam(output)
       const lambda = new Lambda(output)
+      const mutations = model.type.Mutation ? model.type.Mutation : {};
+      const queries = model.type.Query ? model.type.Query : {};
+      const mutationsAndQueries = { ...mutations, ...queries };
       testClass.ImportsForTest(output)
       ts.writeLine()
       testClass.initializeTest("Lambda Attach With Dynamodb Constructs Test",()=>{
         ts.writeLine()
         iam.DynodbIdentifierFromStack()
         ts.writeLine()
-        lambda.initializeTestForLambdaWithDynodb(API_NAME)
+        if (LAMBDA_STYLE === "single lambda") {
+          let funcName = `${API_NAME}Lambda`
+          lambda.initializeTestForLambdaWithDynodb(funcName,"main")
+        }
+        else if(LAMBDA_STYLE === "multiple lambda") {
+          Object.keys(mutationsAndQueries).forEach((key) => {
+            let funcName = `${API_NAME}Lambda${key}`
+            lambda.initializeTestForLambdaWithDynodb(funcName,key);
+            ts.writeLine();
+          })
+        }
         ts.writeLine()
         iam.lambdaServiceRoleTest()
         ts.writeLine()
-        iam.lambdaIdentifierFromStack()
+
+        if(LAMBDA_STYLE === "single lambda"){
+          iam.lambdaServiceRolePolicyTestForDynodb(1)
+        }
+        else if(LAMBDA_STYLE === "multiple lambda"){
+          iam.lambdaServiceRolePolicyTestForDynodb(Object.keys(mutationsAndQueries).length)
+        }
         ts.writeLine()
-        iam.roleIdentifierFromLambda()
-        ts.writeLine()
-        iam.lambdaServiceRolePolicyTestForDynodb()
-        ts.writeLine()
+        
       },output)
     }
   )}
