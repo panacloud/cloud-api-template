e8f1d628d0c2d56d07a27efaf2462cdde9a5a6c4
Muh-Hasan <muh.hasan.git@gmail.com>
2021-08-04 02:59:54 +0500

    update gitignore

--- codeGenerator/bin/index.ts
+++ codeGenerator/bin/index.ts
@@ -3,27 +3,36 @@ import { Generator } from "@yellicode/templating";
 import { TypeScriptWriter } from "@yellicode/typescript";
 const model = require("../../model.json");
 const { USER_WORKING_DIRECTORY } = model;
-const _ = require("lodash")
+const _ = require("lodash");
 
 Generator.generate(
-    {
-      outputFile: `../../../../bin/${USER_WORKING_DIRECTORY}.ts`,
-    },
-    (output: TextWriter) => {
-        const ts = new TypeScriptWriter(output);
-        ts.writeImports("aws-cdk-lib", "cdk");
-        ts.writeImports(`../lib/${USER_WORKING_DIRECTORY}-stack`, [`${_.upperFirst(_.camelCase(USER_WORKING_DIRECTORY))}Stack`])
+  {
+    outputFile: `../../../../bin/${USER_WORKING_DIRECTORY}.ts`,
+  },
+  (output: TextWriter) => {
+    const ts = new TypeScriptWriter(output);
+    ts.writeImports("aws-cdk-lib", "cdk");
+    ts.writeImports(`../lib/${USER_WORKING_DIRECTORY}-stack`, [
+      `${_.upperFirst(_.camelCase(USER_WORKING_DIRECTORY))}Stack`,
+    ]);
 
-        ts.writeVariableDeclaration(
-            {
-              name: `app`,
-              typeName: "cdk.App",
-              initializer: () => {
-                ts.writeLine(`new cdk.App()`);
-              },
-            },
-            "const"
-        );
+    ts.writeVariableDeclaration(
+      {
+        name: `app`,
+        typeName: "cdk.App",
+        initializer: () => {
+          ts.writeLine(`new cdk.App()`);
+        },
+      },
+      "const"
+    );
 
-        output.writeLineIndented(`new ${_.upperFirst(_.camelCase(USER_WORKING_DIRECTORY))}Stack(app, "${_.upperFirst(_.camelCase(USER_WORKING_DIRECTORY))}Stack", {});`)
-})
+    output.writeLineIndented(
+      `new ${_.upperFirst(
+        _.camelCase(USER_WORKING_DIRECTORY)
+      )}Stack(app, "${_.upperFirst(
+        _.camelCase(USER_WORKING_DIRECTORY)
+      )}Stack", {});`
+    );
+  }
+);
