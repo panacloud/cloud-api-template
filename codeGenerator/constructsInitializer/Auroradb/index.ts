b76a4ee58a1ee2f368d58ae40e27e135a2f48654
TanzeelTasleem <tanzeeltasleem648@gmail.com>
2021-08-06 14:36:59 +0500

    feat: add auroradb Construct seperate initialiazer #13

---
+++ codeGenerator/constructsInitializer/Auroradb/index.ts
@@ -0,0 +1,56 @@
+import { TextWriter } from "@yellicode/core";
+import { Generator } from "@yellicode/templating";
+import { PropertyDefinition, TypeScriptWriter } from "@yellicode/typescript";
+import { CONSTRUCTS, DATABASE, LAMBDA } from "../../../cloud-api-constants";
+import { AuroraServerless } from "../../../Constructs/AuroraServerless";
+import { Cdk } from "../../../Constructs/Cdk";
+import { Ec2 } from "../../../Constructs/Ec2";
+import { Iam } from "../../../Constructs/Iam";
+import { auroradbPropertiesHandler, auroradbPropertiesInitializer } from "./function";
+const model = require("../../../model.json");
+const {database} = model.api;
+
+if(database && database === DATABASE.auroraDb){
+Generator.generateFromModel({  outputFile: `../../../../../lib/${CONSTRUCTS.auroradb}/index.ts`,},
+    (output: TextWriter,model: any) => {
+        const ts = new TypeScriptWriter(output)
+        const { apiName, lambdaStyle, database } = model.api;
+        const cdk = new Cdk(output);
+        const ec2 = new Ec2(output);
+        const aurora = new AuroraServerless(output);
+        const iam = new Iam(output);
+        const auroradbProperties:PropertyDefinition[] = auroradbPropertiesHandler()
+        cdk.importsForStack(output)
+        ts.writeImports("aws-cdk-lib", ["Duration"]);
+        aurora.importRds(output);
+        ec2.importEc2(output);
+        ts.writeLine()
+      
+        cdk.initializeConstruct(CONSTRUCTS.auroradb,undefined,()=>{
+            ec2.initializeVpc(apiName, output);
+            ts.writeLine();
+            aurora.initializeAuroraCluster(apiName, `${apiName}_vpc`, output);
+            ts.writeLine();
+            iam.serviceRoleForLambda(apiName, output, [
+              "AmazonRDSDataFullAccess",
+              "service-role/AWSLambdaVPCAccessExecutionRole",
+            ]);
+            ts.writeLine();
+            ts.writeVariableDeclaration(
+              {
+                name: `${apiName}_secret`,
+                typeName: "",
+                initializer: () => {
+                  ts.writeLine(`${apiName}_db.secret?.secretArn || "secret"`);
+                },
+              },
+              "const"
+            );
+            ts.writeLine();
+            aurora.connectionsAllowFromAnyIpv4(`${apiName}_db`);
+            ts.writeLine()
+            auroradbPropertiesInitializer(output,apiName)
+        },output,undefined,auroradbProperties)
+    }
+  );
+}
