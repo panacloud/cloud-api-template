450ebd3ae680ab651bf0f7238f786c2dbd55990b
shariqanwar20 <shariq.anwar@nixorcollege.edu.pk>
2021-08-12 22:05:01 +0500

    added ApiGateway construct && added apiGateway in codegenconfig #16

--- CodeGenerator/ConstructsInitializer/ApiGateway/index.ts
+++ CodeGenerator/ConstructsInitializer/ApiGateway/index.ts
@@ -1,7 +1,7 @@
 import { TextWriter } from "@yellicode/core";
 import { Generator } from "@yellicode/templating";
 import { TypeScriptWriter } from "@yellicode/typescript";
-import { CONSTRUCTS } from "../../../cloud-api-constants";
+import { APITYPE, CONSTRUCTS } from "../../../cloud-api-constants";
 import { ApiGateway } from "../../../Constructs/ApiGateway";
 import { Cdk } from "../../../Constructs/Cdk";
 const model = require("../../../model.json");
@@ -11,22 +11,22 @@ Generator.generateFromModel(
     outputFile: `../../../../../lib/${CONSTRUCTS.apigateway}/index.ts`,
   },
   (output: TextWriter, model: any) => {
-    const ts = new TypeScriptWriter(output);
-    const cdk = new Cdk(output)
-    const apigw = new ApiGateway(output);
-    cdk.importsForStack(output);
-    apigw.importApiGateway(output)
+    const { apiName, apiType } = model.api;
+    if (apiType === APITYPE.rest) {
+      const ts = new TypeScriptWriter(output);
+      const cdk = new Cdk(output);
+      const apigw = new ApiGateway(output);
+      cdk.importsForStack(output);
+      apigw.importApiGateway(output);
 
-    const { apiName } = model.api;
-
-    const props = [
+      const props = [
         {
           name: `${apiName}_lambdaFn`,
           type: "lambda.Function",
         },
-      ]
+      ];
 
-    cdk.initializeConstruct(
+      cdk.initializeConstruct(
         `${CONSTRUCTS.apigateway}`,
         "ApiGatewayProps",
         () => {
@@ -36,6 +36,6 @@ Generator.generateFromModel(
         output,
         props
       );
-
+    }
   }
-)
+);
