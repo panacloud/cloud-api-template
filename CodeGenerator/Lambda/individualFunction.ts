7e8fec94870b1d3314e71d9238ba6833c28f7fdb
shariqanwar20 <shariq.anwar@nixorcollege.edu.pk>
2021-08-12 14:24:47 +0500

    added lambda rest api functionality in individual function #16

--- CodeGenerator/Lambda/individualFunction.ts
+++ CodeGenerator/Lambda/individualFunction.ts
@@ -2,35 +2,56 @@ import { TextWriter } from "@yellicode/core";
 import { Generator } from "@yellicode/templating";
 import { LambdaFunction } from "../../Constructs/Lambda/lambdaFunction";
 import { APITYPE, LAMBDA } from "../../cloud-api-constants";
+import * as SwaggerParser from '@apidevtools/swagger-parser';
 const jsonObj = require(`../../model.json`);
 const { lambdaStyle, apiType } = jsonObj.api;
 
-if (lambdaStyle === LAMBDA.single && apiType === APITYPE.graphql) {
-  if (jsonObj?.type?.Query) {
-    Object.keys(jsonObj.type.Query).forEach((key) => {
-      Generator.generate(
-        {
-          outputFile: `../../../../lambda-fns/${key}.ts`,
-        },
-        (writer: TextWriter) => {
-          const lambda = new LambdaFunction(writer);
-          lambda.helloWorldFunction(key);
-        }
-      );
-    });
+if (lambdaStyle === LAMBDA.single) {
+  if(apiType === APITYPE.graphql) {
+    if (jsonObj?.type?.Query) {
+      Object.keys(jsonObj.type.Query).forEach((key) => {
+        Generator.generate(
+          {
+            outputFile: `../../../../lambda-fns/${key}.ts`,
+          },
+          (writer: TextWriter) => {
+            const lambda = new LambdaFunction(writer);
+            lambda.helloWorldFunction(key);
+          }
+        );
+      });
+    }
+  
+    if (jsonObj.type.Mutation) {
+      Object.keys(jsonObj.type.Mutation).forEach((key) => {
+        Generator.generate(
+          {
+            outputFile: `../../../../lambda-fns/${key}.ts`,
+          },
+          (writer: TextWriter) => {
+            const lambda = new LambdaFunction(writer);
+            lambda.helloWorldFunction(key);
+          }
+        );
+      });
+    }
   }
-
-  if (jsonObj.type.Mutation) {
-    Object.keys(jsonObj.type.Mutation).forEach((key) => {
-      Generator.generate(
-        {
-          outputFile: `../../../../lambda-fns/${key}.ts`,
-        },
-        (writer: TextWriter) => {
-          const lambda = new LambdaFunction(writer);
-          lambda.helloWorldFunction(key);
-        }
-      );
-    });
+  else {
+    SwaggerParser.validate(JSON.parse(jsonObj.openApiDef), (err, api) => {
+      if (err) {
+        console.error(err);
+      }
+      else {
+        Object.keys(api.paths).forEach((path, i) => {
+          console.log("PathName => ", path);
+          Object.keys(api.paths.path).forEach((methodName) => {
+            console.log("Lambda file name => ", api.paths.path.methodName.operationId);
+            api.paths.path.methodName.parameters.forEach((param: any) => {
+              console.log("InputParam => ", param.name);
+            })
+          })
+        })
+      }
+    })
   }
 }
